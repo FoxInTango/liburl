3af03d51f8c04cb9b75639e5d33bd726d2906d7a
Jack.WSL <foxintango@yeah.net>
2023-06-14 22:25:27 +0800

    alpine

--- src/url.h
+++ src/url.h
@@ -6,9 +6,45 @@
 
 EXTERN_C_BEGIN
 namespaceBegin(foxintango)
+/** https://developer.mozilla.org/zh-CN/docs/Learn/Common_questions/Web_mechanics/What_is_a_URL
+ */
+class foxintangoAPI URLAuthority{
+public:
+    String domain;
+    String port;
+public:
+    URLAuthority();
+    ~URLAuthority();
+public:
+    operator String();
+};
+
+class foxintangoAPI URLPath{
+public:
+    Array<String> nodes;
+public:
+    URLPath();
+    URLPath(const String& path);
+    ~URLPath();
+public:
+    operator String();
+};
+
+class foxintangoAPI URLParams{
+public:
+    Map<String,String> params;
+public:
+    URLParams();
+    ~URLParams();
+public:
+    operator String();
+};
 
 class foxintangoAPI URL {
-
+protected:
+    String scheme;
+    URLAuthority authority;
+    URLPath path;
 public:
     /** 定位符类型
      *  
@@ -28,7 +64,7 @@ public:
     URL(const String& url);
     ~URL();
 public:
-    const String& string();
+    operator String();
 };
 
 namespaceEnd
