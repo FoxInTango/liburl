0b83be37dc06d436cc4071dc50490b5321aefbff
Jack.WSL <foxintango@yeah.net>
2023-06-24 17:08:33 +0800

    alpine

--- src/url.cpp
+++ src/url.cpp
@@ -1,6 +1,27 @@
 #include "url.h"
 using namespace foxintango;
 
+URLAuthority::URLAuthority(){}
+URLAuthority::~URLAuthority(){}
+URLAuthority::operator foxintango::String(){
+    return String();
+}
+
+URLPath::URLPath(){}
+URLPath::URLPath(const String& path){}
+URLPath::~URLPath(){}
+URLPath::operator foxintango::String(){
+    return String();
+}
+
+URLParams::URLParams(){}
+URLParams::~URLParams(){}
+URLParams::operator foxintango::String(){
+    return String();
+}
+
+
+
 URL::URL(){
 }
 
@@ -8,4 +29,8 @@ URL::URL(const String& url) {
 }
 
 URL::~URL(){
+}
+
+URL::operator foxintango::String(){
+    return String();
 }
